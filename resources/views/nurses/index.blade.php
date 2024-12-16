10bb0ab8586e1370f75e86a0637ce05658d1aa3d
WebEdge Technologies <alhassan.mohammedga@gmail.com>
2024-12-16 17:34:19 +0000

    Nurses View Initial

---
+++ resources/views/nurses/index.blade.php
@@ -0,0 +1,50 @@
+    <x-app-layout>
+               <div class="container-xxl flex-grow-1 container-p-y">    
+                  <h4 class="py-3 mb-4">
+                    <span class="text-muted fw-light">Nurses Notes/</span> List
+                  </h4>
+                  <div class="app-ecommerce-category">
+                  <div class="card">
+                    <div class="card-datatable table-responsive">
+                      <div style="margin:15px">
+                        <a class="btn btn-primary me-sm-3 me-1" href="#">Nurses Notes</a>
+                      </div>
+                      <table class="datatables-category-list table border-top" id="product_list">
+                        <thead>
+                          <tr>
+                            <th>Sn</th>
+                            <th>Diagnosis</th>
+                            <th>Class</th>
+                            <th>ICD-10</th>
+                            <th class="text-nowrap text-sm-end">GDRG &nbsp;</th>
+                            <th>NHIS Cover</th>
+                            <th>Status</th>
+                            <th class="text-lg-center">Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                           
+                                
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>Sn</th>
+                            <th>Diagnosis</th>
+                            <th>Class</th>
+                            <th>ICD-10</th>
+                            <th>GDRG &nbsp;</th>
+                            <th>NHIS Cover</th>
+                            <th>Status</th>
+                            <th class="text-lg-center">Actions</th>
+                          </tr>
+                        </tfoot>
+                      </table>
+                    </div>
+                  </div>   
+             </div>
+          </div>
+
+          <!-- product modal form -->
+
+          <!-- end of product modal form -->
+    </x-app-layout>
