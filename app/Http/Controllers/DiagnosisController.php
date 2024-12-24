10f6300eec3386f99c08b794806fd4909d9fccdc
WebEdge Technologies <alhassan.mohammedga@gmail.com>
2024-12-24 17:24:44 +0000

    Diagnosis migration upate

--- app/Http/Controllers/DiagnosisController.php
+++ app/Http/Controllers/DiagnosisController.php
@@ -9,17 +9,11 @@ class DiagnosisController extends Controller
 {
     public function index()
     {
+        // // ->lockForUpdate() 
         $diagnosis = Diagnosis::where('archived', 'No')->where('status', '=','Active')
         ->orderBy('diagnosis.diagnosis', 'asc') 
         ->get();
-
-        // $item = Product::rightJoin('product_type', 'product_type.product_type_id', '=', 'products.product_type_id')
-        // ->rightjoin('stores', 'stores.store_id', '=', 'products.store_id')
-        // ->where('products.archived', 'No')
-        // ->select('products.*','product_type.*', 'stores.*')
-        // ->orderBy('products.added_date', 'asc')
-        // // ->lockForUpdate() 
-        // ->get();
+        // ->paginate(20);
 
         return view('diagnosis.index', compact('diagnosis'));
     
